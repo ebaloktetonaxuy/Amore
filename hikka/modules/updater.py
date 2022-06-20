0e21ea7d1fae7a5cef6665d824f6ba24aeb4c713
Amore <103936585+AmoreForever@users.noreply.github.com>
2022-06-20 23:17:16 +0500

    Update updater.py

--- hikka/modules/updater.py
+++ hikka/modules/updater.py
@@ -117,7 +117,7 @@ class UpdaterMod(loader.Module):
         self.config = loader.ModuleConfig(
             loader.ConfigValue(
                 "GIT_ORIGIN_URL",
-                "https://github.com/AmoreForever/NinoEZ",
+                "https://github.com/AmoreForever/Nino",
                 lambda: self.strings("origin_cfg_doc"),
                 validator=loader.validators.Link(),
             )
